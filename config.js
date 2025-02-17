bf6e04fdc0e51692a1a2fe5eeddf1fbd6ecaaaab
PayoeboiMalvinTech <malvink002@gmail.com>
2025-02-17 23:23:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'MALVIN-XD~FkIQFKrL#yWFMOmZQROxLSeqKtXhsyeymTvwfmM_V1O5OtUoj0JU' : process.env.SESSION_ID,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'PayoeboiMalvinTech' : process.env.GITHUB_USER_NAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN,        
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '263714757857' : process.env.OWNER_NUMBER
 };
